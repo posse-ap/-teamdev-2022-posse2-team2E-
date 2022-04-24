392599d3818bcbd598d310e7c184871c2b8fe9e8
akira1515 <akira.kobayashi1515@gmail.com>
2022-04-24 18:06:05 +0900

    トップページ　最低限の見た目完成

---
+++ .history/src/js/user/main_20220419172050.js
@@ -0,0 +1,20 @@
+
+// 絞り込み
+var widget = document.getElementById('js-filter');
+var checkboxes = widget.querySelectorAll('.filter-cond input[type="checkbox"]');
+var checkedList = [];
+var filter = function () {
+    checkedList = [];
+
+    Array.prototype.forEach.call(checkboxes, function (input) {
+        if (input.checked) {
+            checkedList.push(input.value);
+        }
+    });
+
+    widget.setAttribute('data-filter-view', checkedList.join(' '));
+};
+
+Array.prototype.forEach.call(checkboxes, function (checkbox) {
+    checkbox.addEventListener('change', filter);
+});
