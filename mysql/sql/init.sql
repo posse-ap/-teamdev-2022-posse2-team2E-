5a2f505b9a12149aabf00bc0cebbe93778d1f956
kashiken4869 <kashiken4646@gmail.com>
2022-05-14 12:59:17 +0900

    セッション接続成功 あとは企業ごとに学生情報をソートする　ログアウトも実装完了

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -5,6 +5,7 @@ USE shukatsu;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  agent_name VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -13,12 +14,11 @@ CREATE TABLE users (
 
 -- INSERT INTO users SET email='testtest@gmail.com', password=sha1('password');
 
-INSERT INTO users SET email='testtest@gmail.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
-INSERT INTO users SET email='test@icloud.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
-INSERT INTO users SET email='test@neko.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
-INSERT INTO users SET email='test@nya.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
-INSERT INTO users SET email='test@hiii.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
-
+INSERT INTO users SET agent_name='kashiken', email='testtest@gmail.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
+INSERT INTO users SET agent_name='kashiken', email='test@icloud.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
+INSERT INTO users SET agent_name='kashiken', email='test@neko.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
+INSERT INTO users SET agent_name='kashiken', email='test@nya.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
+INSERT INTO users SET agent_name='kashiken', email='test@hiii.com', password='$2y$10$jFWlJ.8RvTWwNd1ZYv4NReC4KmMqgb804u./LDVDrz1Bb0w7h781.';
 -- INSERT INTO users (email, password) VALUES ('testtest@gmail.com', sha1('password'));
 -- INSERT INTO users (email, password) VALUES ('testtest@icloud.com', sha1('password'));
 -- INSERT INTO users (email, password) VALUES ('testtest@nya.com', sha1('password'));
--- src/agent/agent_login.php
+++ src/agent/agent_login.php
@@ -1,41 +1,119 @@
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/db_connect.php");
+session_start();
 
-
-// ログイン済みかを確認
-if (isset($_SESSION['USER'])) {
-    header('Location: agent_students_all.php'); // ログインしていればresult-index.phpへリダイレクトする
-    exit; // 処理終了
-}
+// // ログイン済みかを確認
+// if (isset($_SESSION['login'])) {
+//     session_regenerate_id(TRUE);
+//     header('Location: agent_students_all.php'); // ログインしていればresult-index.phpへリダイレクトする
+//     exit; // 処理終了
+// }
 
 if (isset($_POST["submit"])) {
 
-try {
-    $dbh = new PDO("mysql:host=db; dbname=shukatsu; charset=utf8", "$user", "$password");
+    try {
+        $db = new PDO("mysql:host=db; dbname=shukatsu; charset=utf8", "$user", "$password");
 
-    $stmt = $dbh->prepare('SELECT * FROM users WHERE email = :email');
+        $stmt = $db->prepare('SELECT * FROM agents WHERE login_email = ?');
+        // $stmt = $db->prepare('SELECT * FROM agents WHERE login_email = :email limit 1');
+        // $stmt->execute(array(':email' => $_POST['email']));
+        // $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $stmt->execute(array(':email' => $_POST['email']));
+        $stmt->bindParam(1, $_POST['email'], PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($_POST['pass'], $result['login_pass'])) {
+            // $redirect[1] = "agent_students_all.php?id=1";
+            // if( isset($_GET['id']) ){
+            //     $id = intval($_GET['id']);
+            //     header("Location:$redirect[$id]");
+            // }
+            session_regenerate_id(TRUE); //セッションidを再発行
+            $_SESSION["login"] = $_POST['email']; //セッションにログイン情報を登録
+            header('Location: agent_students_all.php');
+        } else {
+            $msg = 'メールアドレスもしくはパスワードが間違っています。';
+            var_dump(password_hash("miyuki", PASSWORD_DEFAULT));
 
-    if (password_verify($_POST['pass'], $result['password'])) {
-        header('Location: agent_students_all.php');
-    } else {
-        $msg = 'メールアドレスもしくはパスワードが間違っています。';
-        // var_dump(password_hash("password", PASSWORD_DEFAULT));
-        // var_dump($result['password']);
-        // var_dump($_POST['pass']);
-        // var_dump($_POST['email']);
-        // var_dump($result['email']);
-        
+            echo "わあああああああああああ";
 
+            var_dump($_POST['pass']);
+
+            echo "わあああああああああああ";
+
+            var_dump($result['login_pass']);
+
+            echo "わあああああああああああ";
+
+            var_dump($_POST['email']);
+
+            echo "わあああああああああああ";
+
+            var_dump($result['login_email']);
+        }
+    } catch (PDOException $e) {
+        echo "もう一回";
+        $msg = $e->getMessage();
+        exit;
     }
-} catch (PDOException $e) {
-    echo "もう一回";
-    $msg = $e->getMessage();
-    // die();
 }
-}
+
+// $id = $_GET['id'];
+// echo $id;
+
+// if (empty($id)) {
+//  exit('IDが不正です。');
+// }
+
+// $error = [];
+// $email = '';
+// $password = '';
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//   $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+//   $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+// if ($email === '' || $password === '') {
+//   $error['login'] = 'blank';
+// } else {
+//   // ログインチェック
+//   $db = dbconnect();
+//   $stmt = $db->prepare('select id, name, password from members where email=? limit 1');
+//   if (!$stmt) {
+//     die($db->error);
+//   }
+
+// $stmt->bind_param('s', $email);
+// $success = $stmt->execute();
+// if (!$success) {
+//   die($db->error);
+// }
+
+// $stmt->bind_result($id, $name, $hash);
+// $stmt->fetch();
+
+//     if (password_verify($password, $hash)) {
+//       // ログイン成功
+//       session_regenerate_id();
+//       $_SESSION['id'] = $id;
+//       $_SESSION['name'] = $name;
+//       header('Location: index.php');
+//       exit();
+//     } else {
+//       $error['login'] = 'failed';
+//     }
+//   // }
+// }
+
+// $id_stmt = $db->prepare('SELECT * FROM users where id = :id');
+// $id_stmt->bindValue(':id', (int)$id, PDO::PARAM_INT);
+// //SQL実行
+// $id_stmt->execute();
+// //結果を取得
+// $result = $id_stmt->fetch(PDO::FETCH_ASSOC);
+
+// if(!$result) {
+//     exit('データがありません。');
+// }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -53,7 +131,7 @@ try {
 </head>
 
 <body>
-    <header>
+    <!-- <header> -->
         <h1>
             <p><span>CRAFT</span>by boozer</p>
         </h1>
@@ -62,20 +140,20 @@ try {
     <div class="agent_login_info">
         <h2 class="agent_login_title">エージェント企業用画面</h2>
         <form action="" method="post">
-        <h3 class="pass_wrong"><?php echo $msg; ?></h3>
+            <h3 class="pass_wrong"><?php echo $msg; ?></h3>
             <p>
-                <p class="agent_login_label">メールアドレス</p>
-                <input type="text" name="email" required>
+            <p class="agent_login_label">メールアドレス</p>
+            <input type="text" name="email" value="<?php echo h($email); ?>" required>
             </p>
             <p>
-                <p class="agent_login_label">パスワード</p>
-                <input type="password" name="pass">
+            <p class="agent_login_label">パスワード</p>
+            <input type="password" name="pass" value="<?php echo h($password); ?>" required>
             </p>
-            <input type="submit" name="submit" value="ログイン" required>
+            <input type="submit" name="submit" value="ログイン">
         </form>
         <!-- <a href="inquiry.php" class="pass_forget">パスワードをお忘れの方はこちら</a> -->
     </div>
-        <div class="inquiry">
+    <!-- <div class="inquiry"> -->
         <p>お問い合わせは下記の連絡先にお願いいたします。
             <br>craft運営 boozer株式会社事務局
             <br>TEL:080-3434-2435
